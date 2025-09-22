4b5ee56723a1d35f0721f92582bf0848802abe0e
Soham Morchapure <sohamwork21@gmail.com>
2025-09-22 23:44:48 +0530

    initial commit - m

---
+++ src/main/java/module-info.java
@@ -0,0 +1,17 @@
+module com.example.javafx_sudoku_app {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+    requires com.fasterxml.jackson.databind;
+
+    opens sudoku to javafx.fxml;
+    exports sudoku;
+}
